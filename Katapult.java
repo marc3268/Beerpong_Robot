49c4ab89580990d1c38ab0778e9b1963a3fce3cf
marc3268 <47645452+marc3268@users.noreply.github.com>
2019-04-21 21:34:32 +0200

    Add files via upload

---
+++ Katapult.java
@@ -0,0 +1,137 @@
+package tir_16;
+
+import lejos.hardware.Sound;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.utility.Delay;
+
+public class Katapult {
+
+	static EV3LargeRegulatedMotor Kast = new EV3LargeRegulatedMotor(MotorPort.B);
+	static EV3LargeRegulatedMotor Kast1 = new EV3LargeRegulatedMotor(MotorPort.C);
+
+	// Mål i cm
+	static double KOP_HØJDE = 9;
+	static double KOP_RADIUS = 0.5;
+
+	static double HØJDE_SKUD = 40;
+	static double ARM_LÆNGDE = 22;
+	static double AFSTAND_SENSOR = 31; // fra slippepunkt til sensor
+	static double vinkel = Math.toRadians(40);
+
+	static int grader = 100; // hvor langt lademekanismen skal rotere
+	static double g = -981; // tyngdekraft i cm/s^2
+	static double speed;
+	static int acc; // enhed: degress/second/second, en kraft
+
+	static double afstand = Robotten.Afstand_Kørt;
+	static double x = afstand + AFSTAND_SENSOR + Robotten.sensor_stop_cm + KOP_RADIUS; // totale afstand i x retning
+
+	
+	// metode til at finde tiden bolden skal være i luften før den rammer koppen,
+	// udledt fra skrå kast formlerne se evt bilag
+	
+	static double Tid() {
+		double p1 = KOP_HØJDE - (x * Math.sin(vinkel) / Math.cos(vinkel)) - HØJDE_SKUD;
+		double p2 = p1 / (0.5 * g);
+		double p3 = Math.sqrt(p2);
+
+		return p3;
+
+	}
+
+	static double Hastighed_cm_sek() {
+		double V_x = x / Tid();
+		return V_x / Math.cos(vinkel);
+	}
+
+	static double motor_speed() {
+		double omkreds = 2 * Math.PI * ARM_LÆNGDE;
+		speed = (Hastighed_cm_sek() / omkreds) * 360;
+		return speed;
+	}
+
+	// udledt fra potensregrssion af data fra forsøg. se evt bilag
+	static void set_acc() {
+		acc = (int) (2.38 * Math.pow(Hastighed_cm_sek(), 1.27));
+	}
+
+	static void SKYD() {
+		Kast.setSpeed(100);
+		Kast1.setSpeed(100);
+
+		Kast.setAcceleration(1000);
+		Kast1.setAcceleration(1000);
+
+		Kast.rotate(grader, true);
+		Kast1.rotate(grader, true);
+
+		Delay.msDelay(3000);
+		Sound.beepSequenceUp();
+
+		Kast.setSpeed((int) speed);
+		Kast1.setSpeed((int) speed);
+		Kast.setAcceleration(acc);
+		Kast1.setAcceleration(acc);
+
+		Delay.msDelay(4000);
+
+		Kast.rotate(-grader + 5, true);
+		Kast1.rotate(-grader + 5, false);
+		
+		Delay.msDelay(1000);
+
+	}
+
+	//Metoden fungere ikke med harware
+	static void AutoReset() {
+		Kast.setSpeed(20);
+		Kast1.setSpeed(20);
+		Kast.setAcceleration(500);
+
+		Kast1.setAcceleration(500);
+		Kast.setStallThreshold(3, 150);
+		Kast1.flt();
+		// Kast1.setStallThreshold(2, 100);
+
+		while (!Kast.isStalled()) // ||(!Kast1.isStalled()))
+		{
+			Kast.rotate(-7, true);
+			Delay.msDelay(100);
+		}
+		Kast.stop(true);
+		Kast1.stop(true);
+		Delay.msDelay(2000);
+
+		Kast.setStallThreshold(50, 1000);
+
+	}
+
+	static void ManueltReset() {
+
+		Kast.rotate(-2, true);
+		Kast1.rotate(-2, true);
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+/*
+ * static double Tid_y(){
+ * 
+ * double V_x = hastighed_v0_ms * Math.cos(vinkel); double V_y = hastighed_v0_ms
+ * * Math.sin(vinkel);
+ * 
+ * double t=0; double andengradsligning = 0.5*g*t*t+V_y*t+højde_skud-højde_kop;
+ * 
+ * //Determinant double D = (V_y*V_y)-(4*0.5*g*(højde_skud-højde_kop));
+ * 
+ * double T1=(-V_y+Math.sqrt(D))/(2*0.5*g); double
+ * T2=(-V_y-Math.sqrt(D))/(2*0.5*g);
+ * 
+ * if (T1<0) {T1=T2;} return T1; }
+ */
---
+++ Robotten.java
@@ -0,0 +1,145 @@
+package tir_16;
+
+import lejos.hardware.Button;
+import lejos.hardware.Sound;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.utility.Delay;
+
+public class Robotten {
+
+	static EV3LargeRegulatedMotor Hjul_Højre = new EV3LargeRegulatedMotor(MotorPort.A);
+	static EV3LargeRegulatedMotor Hjul_Venstre = new EV3LargeRegulatedMotor(MotorPort.D);
+	static EV3UltrasonicSensor sensor = new EV3UltrasonicSensor(SensorPort.S1);
+
+	static DifferentialPilot pilot = new DifferentialPilot(5.5, 12, Hjul_Højre, Hjul_Venstre, true);
+	final static SampleProvider sp = sensor.getDistanceMode();
+
+	static double sensor_stop = 0.2; // afstand robotten stopper fra glasset, ganges med 100 for cm
+	static double sensor_stop_cm = sensor_stop * 100;
+
+	static float Afstand_Kørt; // variable der lagre hvor langt robotten har kørt
+
+	// metode til at køre frem indtil vi når en kop, samt gemme afstanden som
+	// variablen, 'Afstand_KØRT
+
+	
+	//metode: robotten kører frem og stopper når den er 20cm fra et objekt
+	public static void go() {
+		pilot.setTravelSpeed(7);
+		pilot.travel(-100, true);
+
+		float[] data = new float[1]; // sensor opsamler data og skal lagre det i et array
+
+		while (pilot.isMoving()) {
+			sp.fetchSample(data, 0);
+			System.out.println(data[0]);
+
+			if (data[0] <= sensor_stop && data[0] != 0) {
+				pilot.stop();
+			}
+		}
+
+		System.out.println(" " + -pilot.getMovement().getDistanceTraveled());
+		Afstand_Kørt = -pilot.getMovement().getDistanceTraveled();
+
+	}
+
+
+
+	public static void main(String[] args) {
+
+		Robotten.go();
+		// sensor.disable();   //virker som en lejos bug men robotten looper, hvis sensoren slukkes
+	
+
+		pilot.travel(Afstand_Kørt);
+		
+		Katapult.motor_speed(); // udregner fart baseret på afstand til kop
+		Katapult.set_acc();
+		
+		System.out.println("vektor cm pr. sek: " +(int)Katapult.Hastighed_cm_sek());
+		System.out.println("rotationshastighed, grader:" +(int)Katapult.speed);
+		System.out.println("afstand til kop"+(int)Katapult.x);
+
+		Button.waitForAnyPress();
+
+		
+
+			if (Katapult.Kast.getMaxSpeed() < Katapult.speed) 												
+				{
+					System.out.println("OBS batteriet er ikke ladet nok til at gennemføre skudet");
+					Sound.twoBeeps();
+					Button.waitForAnyPress();
+				}
+	
+		
+		Katapult.SKYD();// affyrer bolden
+		//Katapult.AutoReset();
+		
+		boolean miss = true;
+		
+		//et mode hvori man kan justere katapultens skud uden at starte programmet forfra
+		while (miss) {
+			System.out.println("Ramt? Tryk Enter for at justere, eller afslut med Escape");
+
+			int indput = Button.waitForAnyPress();
+
+			if (indput == Button.ID_ENTER) {
+				System.out.println("nuværende styrke: " + Katapult.speed);
+				System.out.println("tryk op for at forøge styrken med 3%");
+				System.out.println("tryk ned for at mindske styrken med 3%");
+
+				boolean juster = true;
+
+				while (juster) {
+
+					indput = Button.waitForAnyPress();
+
+					if (indput == Button.ID_UP) {
+						Katapult.speed = Katapult.speed * 1.03;
+						Katapult.acc=(int) (Katapult.acc*1.03);
+						System.out.println("ny styrke: " + (int) Katapult.speed);
+						System.out.println("afslut med Enter");
+					}
+
+					
+					else if (indput == Button.ID_DOWN) {
+						Katapult.speed = Katapult.speed * 0.97;
+						Katapult.acc=(int) (Katapult.acc*0.97);
+						System.out.println("ny styrke: " + (int) Katapult.speed);
+						System.out.println("afslut med Enter");
+					}
+					else if (indput==Button.ID_RIGHT)
+					{
+						Katapult.ManueltReset();
+						
+					}
+
+					
+					else if (indput == Button.ID_ENTER) {
+						juster = false;
+					}
+				}
+				System.out.println("skyd");
+			
+			
+			
+				Katapult.SKYD();
+			//	Katapult.AutoReset();
+
+			}
+
+			else if (indput == Button.ID_ESCAPE) {
+				miss = false;
+			}
+
+		}
+		Sound.beepSequenceUp();
+	}
+
+}
